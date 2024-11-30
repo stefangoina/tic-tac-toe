2443d92ee228b73535a169e44b5c275883b683d5
stefan g <116798342+stefangoina@users.noreply.github.com>
2024-11-30 13:57:45 +0200

    added a function to reset the game

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let cards = document.querySelectorAll(".card");
 let startBtn = document.querySelector(".start");
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
+let restart = document.querySelector(".reset");
 // add error
 startBtn.addEventListener("click", playGame());
 function playGame() {
@@ -10,13 +11,16 @@ function playGame() {
   let counter = 0; // x will be even and o will be odd
   cards.forEach((card) => {
     card.addEventListener("click", () => {
-      if (counter % 2 === 0) {
+      if (counter % 2 === 0 && !card.classList.contains("isClicked")) {
+        card.classList.add("isClicked");
         let div = document.createElement("div");
         card.appendChild(div);
         div.textContent = "X";
         div.classList.add("shown");
         startText.textContent = "Player O's Turn";
-      } else {
+      }
+      if (counter % 2 !== 0 && !card.classList.contains("isClicked")) {
+        card.classList.add("isClicked");
         let div = document.createElement("div"); // Create a new div for "O"
         card.appendChild(div);
         div.textContent = "O";
@@ -26,9 +30,17 @@ function playGame() {
       counter++;
     });
   });
+  function reset() {
+    cards.forEach((card) => {
+      card.innerHTML = "";
+      card.classList.remove("isClicked");
+    });
+  }
+
+  restart.addEventListener("click", reset);
 }
 
 //TODO :
 // add reset button
-// make the cards clickable only once
+// make the cards clickable only once (DONE)
 // add the winning conditions
